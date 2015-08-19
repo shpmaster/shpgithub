e3e9934a30c977f619433f8e595d946d9d024f5a
shpmaster <shpgitmaster@gmail.com>
2015-08-20 02:35:45 +0900

    okay

---
+++ gittest_eclipse/src/helloeclipse.java
@@ -0,0 +1,11 @@
+
+public class helloeclipse {
+
+		  public static void main(String args[]) {
+		    System.out.println("Hello World");
+		    System.out.println("Hello eclipse");
+		    //테스트 2차 
+		  }
+		
+
+}
